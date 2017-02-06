3be228c9370b6cf0e5a12ab14da1eca91fd718d0
Spiral <will_whitehouse@icloud.com>
2017-02-06 10:46:03 -0500

    Initial Patch
    Example Added
    Background Canvas Added
    Main JS File

---
+++ js/main.js
@@ -0,0 +1,49 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+
+window.onload = function() {
+    var audio = document.getElementById('audio');
+    var ctx = new AudioContext();
+    var analyser = ctx.createAnalyser();
+    var audioSrc = ctx.createMediaElementSource(audio);
+    audioSrc.connect(analyser);
+    analyser.connect(ctx.destination);
+    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+    var canvas = document.getElementById('canvas')
+        cwidth = canvas.width,
+        cheight = canvas.height - 2,
+        meterWidth = 10,
+        gap = 2,
+        capHeight = 2,
+        capStyle = '#ffffff',
+        meterNum = 3000 / (10 + 2),
+        capYPositionArray = [];
+    ctx = canvas.getContext('2d'),
+    gradient = ctx.createLinearGradient(0, 0, 0, 300);
+    gradient.addColorStop(1, '#EF3D35');
+    gradient.addColorStop(0.5, '#ffffff');
+    gradient.addColorStop(0, '#ffffff');
+    function renderFrame() {
+        var array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+        var step = Math.round(array.length / meterNum);
+        ctx.clearRect(0, 0, cwidth, cheight);
+        for (var i = 0; i < meterNum; i++) {
+            var value = array[i * step];
+            if (capYPositionArray.length < Math.round(meterNum)) {
+                capYPositionArray.push(value);
+            };
+            ctx.fillStyle = capStyle;
+            if (value < capYPositionArray[i]) {
+                ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
+            } else {
+                ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
+                capYPositionArray[i] = value;
+            };
+            ctx.fillStyle = gradient;
+            ctx.fillRect(i * 12 , cheight - value + capHeight, meterWidth, cheight);
+        }
+        requestAnimationFrame(renderFrame);
+    }
+    renderFrame();
+    audio.play();
+};
